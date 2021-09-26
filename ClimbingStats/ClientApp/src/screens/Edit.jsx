634d63677604082d3778b230a37e2ae16886a0a0
Ryan.Naylor <ryannaylor@hotmail.com>
2021-09-26 12:04:52 +0100

    moved pages into screens folder

---
+++ ClimbingStats/ClientApp/src/screens/Edit.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Edit = () => {
+  return (
+    <div>
+      <h1>Update routes</h1>
+      <p>
+        This app is designed to help track what routes you have completed, and
+        your overall progress on routes currently available to climb.
+      </p>
+    </div>
+  );
+};
+
+export default Edit;
--- ClimbingStats/ClientApp/src/screens/Routes.jsx
+++ ClimbingStats/ClientApp/src/screens/Routes.jsx
@@ -3,25 +3,18 @@ import axios from "axios";
 
 const Routes = () => {
   const [state, setState] = useState({
-    colours: [],
-    sections: [],
     routes: [],
     loading: true,
   });
 
   useEffect(() => {
-    populateSummary();
+    populateRoutes();
   }, []);
 
-  const populateSummary = async () => {
-    const response = await fetch("api/route");
-    const colours = await axios.get("api/route/colours");
-    const sections = await axios.get("api/route/sections");
-    const data = await response.json();
+  const populateRoutes = async () => {
+    const routes = await axios("api/route");
     setState({
-      routes: data,
-      colours: colours,
-      sections: sections,
+      routes: routes.data,
       loading: false,
     });
   };
@@ -64,7 +57,6 @@ const Routes = () => {
   return (
     <div>
       <h1 id="tabelLabel">Routes</h1>
-      <p>This component demonstrates fetching data from the server.</p>
       {contents}
     </div>
   );
